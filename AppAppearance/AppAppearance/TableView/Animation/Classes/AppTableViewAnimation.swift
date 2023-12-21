45eb7169d71036242d20d6bd4a88881483f9f73f
Nikita Skripka <>
2023-12-21 14:08:29 +1000

    Add new method for table animation and redo instance methods to static

--- AppAppearance/AppAppearance/TableView/Animation/Classes/AppTableViewAnimation.swift
+++ AppAppearance/AppAppearance/TableView/Animation/Classes/AppTableViewAnimation.swift
@@ -9,7 +9,7 @@ import UIKit
 
 open class AppTableViewAnimation: UITableViewController {
     
-    open func smoothAnimationOfCellsFromBottomToTop(from tableView: UITableView) {
+    public static func smoothAnimationOfCellsFromBottomToTop(from tableView: UITableView) {
         tableView.reloadData()
         
         let cells = tableView.visibleCells
@@ -18,6 +18,26 @@ open class AppTableViewAnimation: UITableViewController {
         
         for cell in cells {
             cell.transform = CGAffineTransform(translationX: 0, y: tableViewHeight)
+            
+            UIView.animate(withDuration: 1.5,
+                           delay: delay * 0.05,
+                           usingSpringWithDamping: 0.8,
+                           initialSpringVelocity: 0, options: .curveEaseInOut) {
+                cell.transform = CGAffineTransform.identity
+            }
+            delay += 1
+        }
+    }
+    
+    public static func smoothAnimationOfCellsFromRightToLeft(from tableView: UITableView) {
+        tableView.reloadData()
+        
+        let cells = tableView.visibleCells
+        let tableViewWidth = tableView.bounds.width
+        var delay: Double = 0
+        
+        for cell in cells {
+            cell.transform = CGAffineTransform(translationX: tableViewWidth, y: 0)
             
             UIView.animate(withDuration: 1.5,
                            delay: delay * 0.05,
